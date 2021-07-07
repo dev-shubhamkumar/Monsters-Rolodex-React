fe8acb7ea157a64ef34ddf4180f3fc504420ad13
Shubham Kumar <developer.shubham07@gmail.com>
2021-07-07 21:21:12 +0530

    Adding Search Box

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -22,7 +23,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters} />
+        <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value }, () => console.log(this.state))} />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
