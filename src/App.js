235d3a64ceb266aed3fdc3d50611dc770b851c97
Shubham Kumar <developer.shubham07@gmail.com>
2021-07-10 12:28:37 +0530

    adding Search Box Functionality using arrow functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -20,6 +21,10 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value }, () => console.log(this.state))
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filterMonsters = monsters.filter(monster =>
@@ -27,7 +32,10 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value }, () => console.log(this.state))} />
+        <SearchBox 
+          placeholder='search monsters' 
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filterMonsters} />
       </div>
     );
